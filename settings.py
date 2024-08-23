b67c439b0142b9d0db1abc68f098f8979a06f521
hussein-srour <hussein.srour@lau.edu>
2024-08-23 19:19:50 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,11 @@
-INSTALLED_APPS = [
+    INSTALLED_APPS = [
     ...
     'rest_framework',
     'myapp',
+
+INSTALLED_APPS += ['corsheaders']
+MIDDLEWARE += ['corsheaders.middleware.CorsMiddleware']
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
+
